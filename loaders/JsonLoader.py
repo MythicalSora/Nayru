7f0914427d9eec7b88ee183230ddee6556b3afcf
Alwin <meijboom.am@gmail.com>
2020-04-28 16:10:56 +0200

    Create basic project structure

---
+++ loaders/JsonLoader.py
@@ -0,0 +1,14 @@
+import json
+
+def __read_file(loc: str):
+    """
+    This function reads a JSON file and returns an object.
+    """
+    with open(loc, 'r') as f:
+        return json.loads(f.read())
+
+def get_config(loc: str):
+    """
+    This function returns a Python object based on the contents of a JSON file.
+    """
+    return __read_file(loc)
